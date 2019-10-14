6f85134851976b07eb27438fc546a953a696e3a8
Amber Jones <amberlovescats14@gmail.com>
2019-10-14 07:56:18 -0500

    save

---
+++ script.js
@@ -0,0 +1,13 @@
+const randomBoolean = Boolean(Math.round(Math.random()));
+const randomPositiveNumber = Math.ceil(Math.random() * 100);
+const randomNegativeNumber = Math.ceil(Math.random() * 100) * -1;
+const exampleObject = { "firstName": "Grace", "lastName": "Hopper"};
+const exampleArray = ['Grace Hopper', 'Ada Lovelace', 'Margaret Hamilton', 'Sandi Metz'];
+const exampleString = "Grace Hopper";
+const randomIndex = Math.floor(Math.random() * exampleArray.length);
+const randomString = exampleArray[randomIndex];
+
+
+
+
+
